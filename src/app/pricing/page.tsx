c0201d5f460144d57f1ba0002d0ea9066193e1cd
Red-Rose515 <Albertchess990919@gmail.com>
2025-05-05 20:51:20 +0300

    update pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -7,74 +7,74 @@ import { Award, Camera, Clock, Image, MapPin, Star, Users, Video } from 'lucide-
 import { PricingCard } from '@/components/pricing/pricing-card';
 import { GradientHeading } from '@/components/ui/graident-heading';
 
-export const plans = [
-  {
-    name: 'Basic',
-    badge: {
-      text: 'New',
-      variant: 'new' as const
-    },
-    description: 'Perfect for individuals',
-    price: 19,
-    // period: 'per session',
-    buttonText: 'Get Credits',
-    buttonVariant: 'outline' as const,
-    features: [
-      { name: '2-hour photo session', icon: Camera },
-      { name: '50 edited digital photos', icon: Image },
-      { name: 'Online gallery', icon: Star },
-      { name: '1 location', icon: Users }
-    ]
-    // footer: {
-    //   text: 'Additional hours available at $100/hour'
-    // }
-  },
-  {
-    name: 'Premium',
-    badge: {
-      text: 'Most popular',
-      variant: 'popular' as const
-    },
-    description: 'Ideal for professionals.',
-    price: 49,
-    buttonText: 'Book Now',
-    buttonVariant: 'outline' as const,
-    features: [
-      { name: 'Full-day coverage (up to 8 hours)', icon: Camera },
-      { name: '200 edited digital photos', icon: Image },
-      { name: 'Online gallery with downloads', icon: Star },
-      { name: 'Multiple locations', icon: Users }
-    ]
-    // footer: {
-    //   text: 'Engagement shoot included',
-    //   link: {
-    //     text: 'Learn more',
-    //     href: '#'
-    //   }
-    // }
-  },
-  {
-    name: 'Commercial',
-    badge: {
-      text: 'Advanced',
-      variant: 'new' as const
-    },
-    description: 'For businesses and professional needs.',
-    price: 99,
-    buttonText: 'Get Credits',
-    buttonVariant: 'outline' as const,
-    features: [
-      { name: 'Full-day photo and video shoot', icon: Camera },
-      { name: '100 edited photos', icon: Image },
-      { name: '5-minute promotional video', icon: Video },
-      { name: 'Commercial usage rights', icon: Star }
-    ]
-    // footer: {
-    //   text: 'Customizable to your specific needs'
-    // }
-  }
-];
 export default function Pricing() {
+  const plans = [
+    {
+      name: 'Basic',
+      badge: {
+        text: 'New',
+        variant: 'new' as const
+      },
+      description: 'Perfect for individuals',
+      price: 19,
+      // period: 'per session',
+      buttonText: 'Get Credits',
+      buttonVariant: 'outline' as const,
+      features: [
+        { name: '2-hour photo session', icon: Camera },
+        { name: '50 edited digital photos', icon: Image },
+        { name: 'Online gallery', icon: Star },
+        { name: '1 location', icon: Users }
+      ]
+      // footer: {
+      //   text: 'Additional hours available at $100/hour'
+      // }
+    },
+    {
+      name: 'Premium',
+      badge: {
+        text: 'Most popular',
+        variant: 'popular' as const
+      },
+      description: 'Ideal for professionals.',
+      price: 49,
+      buttonText: 'Book Now',
+      buttonVariant: 'outline' as const,
+      features: [
+        { name: 'Full-day coverage (up to 8 hours)', icon: Camera },
+        { name: '200 edited digital photos', icon: Image },
+        { name: 'Online gallery with downloads', icon: Star },
+        { name: 'Multiple locations', icon: Users }
+      ]
+      // footer: {
+      //   text: 'Engagement shoot included',
+      //   link: {
+      //     text: 'Learn more',
+      //     href: '#'
+      //   }
+      // }
+    },
+    {
+      name: 'Commercial',
+      badge: {
+        text: 'Advanced',
+        variant: 'new' as const
+      },
+      description: 'For businesses and professional needs.',
+      price: 99,
+      buttonText: 'Get Credits',
+      buttonVariant: 'outline' as const,
+      features: [
+        { name: 'Full-day photo and video shoot', icon: Camera },
+        { name: '100 edited photos', icon: Image },
+        { name: '5-minute promotional video', icon: Video },
+        { name: 'Commercial usage rights', icon: Star }
+      ]
+      // footer: {
+      //   text: 'Customizable to your specific needs'
+      // }
+    }
+  ];
   return (
     <div>
       <div className='container mx-auto space-y-32 px-4 py-16'>
