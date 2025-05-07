8ca1f2ca2532162eeb386d5bb200630d6638d173
Red-Rose515 <Albertchess990919@gmail.com>
2025-05-07 21:20:03 +0300

    update audio player

--- src/app/speeches/page.tsx
+++ src/app/speeches/page.tsx
@@ -8,7 +8,7 @@ const SpeechesPage = () => {
   return (
     <div className='flex w-screen flex-row justify-center'>
       <div className={`flex h-[80vh] w-[85%] flex-row justify-center md:p-4`}>
-        <AudioPlayer id={null} autoplay={true} />
+        <AudioPlayer id={null} autoplay={false} />
       </div>
     </div>
   );
